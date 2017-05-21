eff076f7f16d350efd420c6e45555d2c3ff43553
honwen.chan <honwen.chan@gmail.com>
2017-05-21 17:46:57 +0800

    add cutsom page

--- files/luci/controller/shadowsocks.lua
+++ files/luci/controller/shadowsocks.lua
@@ -30,12 +30,32 @@ function index()
 
 	entry({"admin", "services", "shadowsocks", "log"},
 		call("action_log"),
-		_("System Log"), 40).leaf = true
+		_("System Log"), 90).leaf = true
+
+	if luci.sys.call("command -v /etc/init.d/dnsmasq-extra >/dev/null") ~= 0 then
+		return
+	end
+
+	entry({"admin", "services", "shadowsocks", "gfwlist"},
+		call("action_gfw"),
+		_("GFW-List"), 60).leaf = true
+
+	entry({"admin", "services", "shadowsocks", "custom"},
+		cbi("shadowsocks/gfwlist-custom"),
+		_("Custom-List"), 50).leaf = true
+
 end
 
 function action_log()
 	local fs = require "nixio.fs"
-	local conffile = "/var/log/shadowsocks_watchdog.log" 
+	local conffile = "/var/log/shadowsocks_watchdog.log"
 	local watchdog = fs.readfile(conffile) or ""
 	luci.template.render("admin_status/syslog", {syslog=watchdog})
+end
+
+function action_gfw()
+	local fs = require "nixio.fs"
+	local conffile = "/etc/dnsmasq-extra.d/gfwlist"
+	local gfwlist = fs.readfile(conffile) or ""
+	luci.template.render("shadowsocks/gfwlist", {gfwlist=gfwlist})
 end
